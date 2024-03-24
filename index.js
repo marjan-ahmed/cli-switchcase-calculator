53f325cca0c5a0238049e2b40dcf890eef77a817
Your Name <marjan.ahmed08@yahoo.com>
2024-03-24 23:49:57 +0500

    created cli switch case calculator

---
+++ index.js
@@ -0,0 +1,41 @@
+#! usr/bin/env noden
+import inquirer from "inquirer"; //import this independenci for taking user input
+import chalk from "chalk"; //import this independenci for good UI/UX
+console.log(chalk.greenBright("Welcome to my Calculator (By Marjan Ahmed)"));
+console.log("\n");
+const answers = await inquirer.prompt([{
+        name: "firstNum",
+        type: "number",
+        message: chalk.blueBright("Enter your first number: ")
+    },
+    {
+        name: "operator",
+        type: "list",
+        choices: ["+", "-", "*", "/"],
+        message: chalk.blueBright("Enter any operator: ")
+    },
+    {
+        name: "secondNum",
+        type: "number",
+        message: chalk.blueBright("Enter your second number: ")
+    }
+]);
+const { firstNum, operator, secondNum } = answers;
+let results;
+switch (operator) {
+    case "+":
+        results = firstNum + secondNum;
+        break;
+    case "-":
+        results = firstNum - secondNum;
+        break;
+    case "*":
+        results = firstNum * secondNum;
+        break;
+    case "/":
+        results = firstNum / secondNum;
+        break;
+    default:
+        console.log(chalk.red("Invalid operator"));
+}
+console.log(`${firstNum} ${operator} ${secondNum} = ${results}`);
